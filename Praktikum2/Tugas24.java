d001ca4d01c761788e8e9b76c3fc266f2f7fc39d
Rayyan <99259445+RayyanAzzuhri@users.noreply.github.com>
2022-09-08 02:45:59 +0700

    Penambahan tugas

---
+++ Praktikum2/Tugas24.java
@@ -0,0 +1,10 @@
+public class Tugas24 {
+    public static void main(String[] args) 
+    {
+        for (int gnp =2; gnp<=20; gnp +=2)
+        {
+            System.out.print(gnp+" ");
+        }
+    }
+}
+
