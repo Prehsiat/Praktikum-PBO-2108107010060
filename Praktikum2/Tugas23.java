f64cf36834a5387026bd51359edd31974a428d8b
Rayyan <99259445+RayyanAzzuhri@users.noreply.github.com>
2022-09-08 02:51:53 +0700

    Tugas Prak 07/09/22

---
+++ Praktikum2/Tugas23.java
@@ -0,0 +1,12 @@
+public class Tugas23 {
+    public static void main(String arg[])
+    { 
+        for (int i=6; i>1; i--)
+        {
+            for (int j=5; j>6-i; j--)
+                System.out.print("*");
+                
+            System.out.println();
+        }
+    }
+}
---
+++ Praktikum2/Tugas25.java
@@ -0,0 +1,10 @@
+public class Tugas25 {
+    public static void main(String[] args) 
+    {
+        for (int gnp =2; gnp<=20; gnp +=2)
+        {
+            if (gnp%6==0) continue;
+            System.out.print(gnp+" ");
+        }
+    }
+}
